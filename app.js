422efe1c973db6c580a0f39527116673b22ed3a7
noteezy <martabaranovuch@gmail.com>
2019-10-01 23:48:02 +0300

    add structure of (almost) all pages.

---
+++ app.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const fs = require('fs');
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+	
+	 fs.readFile('index.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+  });
+	
+  // res.statusCode = 200;
+  // res.setHeader('Content-Type', 'text/plain');
+  // res.write(req.url);
+  // res.end();
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
