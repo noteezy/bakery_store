21be72c6f3b4d5f2e080f81fd8b248a86a6a7c5f
noteezy <martabaranovuch@gmail.com>
2019-10-09 11:28:41 +0300

    add some template loading.

--- app1.js
+++ app1.js
@@ -10,7 +10,7 @@ const mySecret = "super_secret001";
 
 const app = express();
 app.use(express.static('static'));
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 
 //app.use(bodyParser.json());
@@ -25,27 +25,44 @@ const pool = new Pool({
   port: 5432,
 })
 
+app.set('views', __dirname+'/templates');
+app.set('view engine', 'ejs');
 
 
 app.get('/',(req,res)=>{
   const token = req.cookies['token'];
   if (!token) res.redirect("/login");
-
   jwt.verify(token, mySecret, function(err, decoded) {
     if (err) res.redirect("/login");
-    res.sendFile(path.join(__dirname+'/static/pages/about.html'));
+    if(decoded['role']==0)res.render('about', { header: 'header_user' });
+    else res.render('about', { header: 'header_admin' });
+  });
+});
+
+app.get('/about',(req,res)=>{
+  var token = req.cookies['token'];
+  if (!token) res.redirect("/login");
+  jwt.verify(token, mySecret, function(err, decoded) {
+    if (err) res.redirect("/login");
+    else{
+    if(decoded['role']==0)res.render('about', { header: 'header_user' });
+    else res.render('about', { header: 'header_admin' });
+    }
   });
 });
 
 
 app.get('/products',(req,res)=>{
-  //console.log(req.cookies['token']);
   const token = req.cookies['token'];
   if (!token) res.redirect("/login");
-
   jwt.verify(token, mySecret, function(err, decoded) {
     if (err) res.redirect("/login");
-    res.sendFile(path.join(__dirname+'/static/pages/products.html'));
+    else{
+    pool.query('SELECT * FROM products', (error, results) => {
+      if(decoded['role']==0)res.render('products', { header: 'header_user' , products: results.rows});
+      else res.render('products-admin', { header: 'header_admin' , products: results.rows});
+      });
+    }
   });
 });
 
@@ -54,14 +71,21 @@ app.get('/products',(req,res)=>{
 SIGNUP API
 ************/
 app.get('/signup',(req,res)=>{
-  res.sendFile(path.join(__dirname+'/static/pages/signup.html'));
+  var token = req.cookies['token'];
+  if (!token){
+     res.sendFile(path.join(__dirname+'/static/pages/signup.html'));
+  }else{
+    jwt.verify(token, mySecret, function(err, decoded) {
+      if (err) res.sendFile(path.join(__dirname+'/static/pages/signup.html'));
+      else res.redirect('/about')
+    });
+  }
 });
 const createUser = (request, response) => {
   const { uname, psw1, psw2 } = request.body
   const pass_hash = bcrypt.hash(psw1, saltRounds, function (err,   hash){
     pool.query('INSERT INTO users (email, password, role) VALUES ($1, $2, $3)', [uname, hash, 0], (error, results) => {
       try{
-        console.log(results);
         if (error) {
           throw "Error while inserting"
         }
@@ -70,8 +94,6 @@ const createUser = (request, response) => {
         console.log(e);
         response.redirect("/signup");}
       })
-    // bcrypt.compare(psw1, hash).then(function(res) {
-    // console.log(res);});
   });
 }
 app.post('/signup', createUser)
@@ -80,25 +102,33 @@ app.post('/signup', createUser)
 LOGIN API
 ************/
 app.get('/login',(req,res)=>{
-  res.sendFile(path.join(__dirname+'/static/pages/login.html'));
+  var token = req.cookies['token'];
+  if (!token)
+  {
+    res.sendFile(path.join(__dirname+'/static/pages/login.html'));
+  }else{
+    jwt.verify(token, mySecret, function(err, decoded) {
+      if (err) res.sendFile(path.join(__dirname+'/static/pages/login.html'));
+      else
+      res.redirect('/about')
+    });
+  }
 });
 const getUser = (request, response) => {
   const {uname, psw} = request.body
-
     pool.query('SELECT * FROM users WHERE email=$1', [uname], (error, results) => {
       try{
         var user_id = results.rows[0]['id'];
         var user_email = results.rows[0]['email'];
         var pass = results.rows[0]['password'];
-
+        var role = results.rows[0]['role'];
         bcrypt.compare(psw, pass, function(err, res) {
-          console.log(res);
           try{
               if (err){
                 throw "Password do not match"
               }
               if (res){
-                var token = jwt.sign({ id: user_id }, mySecret, { expiresIn: 86400 });// expires in 24 hours
+                var token = jwt.sign({ id: user_id, role: role}, mySecret, { expiresIn: 86400 });// expires in 24 hours
                 response.cookie('token', token);
                 response.redirect("/products");
               } else {
@@ -126,6 +156,14 @@ LOGOUT API
 app.get('/logout',(req,res)=>{
   res.clearCookie("token");
   res.redirect("/login");
+});
+
+
+/************
+404 API
+************/
+app.get('*', function(req, res){
+  res.send('what???', 404);
 });
 
 
